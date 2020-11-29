b99adee9fe23a85e57d1f2e6efbdde2c1178403c
Igor Lourenço <theigorlourenco@gmail.com>
2020-11-29 19:44:29 -0300

    Set up database to store data

--- src/consts/commands.ts
+++ src/consts/commands.ts
@@ -5,9 +5,11 @@ export const TASKS = 'TAREFAS'
 export const NEW_TASK = 'NOVA TAREFA'
 export const SIGNIN = 'CADASTRAR'
 export const NEW_TASK_EXPLANATION = `cadastra nova tarefa.\n     • _TAREFA [descricao da tarefa]_.\n     • exemplo: *TAREFA ir fazer algo muito legal*`
-export const DELETE_TASK = 'CONCLUIDA'
-export const DELETE_TASK2 = 'CONCLUÍDA'
-export const DELETE_TASK_EXPLANATION = `marca a tarefa como concluída.\n     • _CONCLUIDA [número da tarefa na lista]_.\n     • exemplo: *CONCLUIDA 1*`
+export const FINISH_TASK = 'CONCLUIDA'
+export const FINISH_TASK2 = 'CONCLUÍDA'
+export const FINISHED_TASKS = 'TAREFAS CONCLUIDAS'
+export const FINISHED_TASKS2 = 'TAREFAS CONCLUÍDAS'
+export const FINISH_TASK_EXPLANATION = `marca a tarefa como concluída.\n     • _CONCLUIDA [número da tarefa na lista]_.\n     • exemplo: *CONCLUIDA 1*`
 
 export const TASKS_EXPLANATION = 'lista suas atividades'
 
@@ -15,6 +17,6 @@ export const HELP_CONTENT = `
 \`\`\`📌 Comandos disponíveis\`\`\` \n\n
 *${TASKS}*: ${TASKS_EXPLANATION} \n
 *${NEW_TASK}*: ${NEW_TASK_EXPLANATION} \n
-*${DELETE_TASK}*: ${DELETE_TASK_EXPLANATION} \n
+*${FINISH_TASK}*: ${FINISH_TASK_EXPLANATION} \n
 *${HELP}*: ${HELP_EXPLANATION} \n
 `
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,34 @@
 import express, {Request, Response} from "express"
-import {DELETE_TASK, DELETE_TASK2, HELP, HELP_CONTENT, NEW_TASK, SIGNIN, TASKS} from "./consts/commands";
+import {
+    FINISH_TASK,
+    FINISH_TASK2,
+    FINISHED_TASKS,
+    FINISHED_TASKS2,
+    HELP,
+    HELP_CONTENT,
+    NEW_TASK,
+    SIGNIN,
+    TASKS
+} from "./consts/commands";
 import {INVALID_COMMAND} from "./consts/messages";
+import {Db, MongoClient, ObjectID} from "mongodb"
+
+const uri = ""
+
+interface Task {
+    _id: string
+    task: string
+    createdAt: Date,
+    finishedAt?: Date
+}
+
+let db: Db | null = null
+
+const dateOptions = {
+    year: 'numeric', month: 'numeric', day: 'numeric',
+    hour: 'numeric', minute: 'numeric', second: 'numeric',
+};
+
 
 const app = express()
 app.use(express.urlencoded({extended: false}));
@@ -17,14 +45,43 @@ app.post('/receive-message', async (request: Request, response: Response) => {
 
     const phoneNumber = messageSender.substring(10)
 
-    if (messageBody.startsWith(TASKS)) {
-        message.body(`oi, ${messageSender}`);
-    } else if (messageBody.startsWith(SIGNIN)) {
-//
-    } else if (messageBody.startsWith(NEW_TASK)) {
-//
-    } else if (messageBody.startsWith(DELETE_TASK) || messageBody.startsWith(DELETE_TASK2)) {
-//
+    if (messageBody.substr(0, FINISHED_TASKS.length) === FINISHED_TASKS || messageBody.substr(0, FINISHED_TASKS2.length) === FINISHED_TASKS2) {
+        const tasks = await db?.collection("tasks").find({active: false}).toArray()
+        let list = ''
+
+        tasks?.map((task: Task, index) => {
+            list += (`*${index + 1}*. ${task.task}\n_Criada em ${new Intl.DateTimeFormat('default', dateOptions).format(task.createdAt)}_\n_Finalizada em ${new Intl.DateTimeFormat('default', dateOptions).format(task.finishedAt)}_\n\n`)
+        })
+
+        message.body(`Oi, Igor, aqui estão suas tarefas concluídas:\n\n${list}`);
+    } else if (messageBody.substr(0, NEW_TASK.length) === NEW_TASK) {
+        const task = messageBody.toString().substring(12)
+        await db?.collection("tasks").insertOne({
+            task,
+            active: true,
+            createdAt: new Date(),
+            finishedAt: null
+        })
+
+        message.body(`A tarefa *_${task}_* foi cadastrada.`)
+    } else if (messageBody.substr(0, FINISH_TASK.length) === FINISH_TASK || messageBody.substr(0, FINISH_TASK2.length) === FINISH_TASK2) {
+        const taskIndex = messageBody.toString().substring(10)
+        const tasks: Task[] | undefined = await db?.collection("tasks").find({active: true}).toArray()
+
+        await db?.collection("tasks").updateOne({_id: new ObjectID(tasks[taskIndex - 1]._id)}, {$set: {active: false}})
+        await db?.collection("tasks").updateOne({_id: new ObjectID(tasks[taskIndex - 1]._id)}, {$set: {finishedAt: new Date()}})
+
+        message.body(`Tarefa *_${tasks[taskIndex - 1].task}_* marcada como concluída.`)
+
+    } else if (messageBody.substr(0, TASKS.length) === TASKS) {
+        const tasks = await db?.collection("tasks").find({active: true}).toArray()
+        let list = ''
+
+        tasks?.map((task: Task, index) => {
+            list += (`*${index + 1}*. ${task.task}\n_Criada em ${new Intl.DateTimeFormat('default', dateOptions).format(task.createdAt)}_\n\n`)
+        })
+
+        message.body(`Olá, Igor, estas são suas tarefas pendentes:\n\n${list}`);
     } else if (messageBody.startsWith(HELP)) {
         message.body(`${HELP_CONTENT}`)
     } else {
@@ -35,5 +92,11 @@ app.post('/receive-message', async (request: Request, response: Response) => {
     response.end(twiml.toString());
 })
 
-app.listen(3333)
+MongoClient.connect(uri, {useUnifiedTopology: true}, (err, client) => {
+    if (err) console.log(err)
+    db = client.db('todo-bot')
+
+    app.listen(3333)
+},)
+
 
